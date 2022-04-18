551342007e189a432074e083416be217a56bc5ab
Matthias Bry <marteo@marteo.fr>
2022-04-18 08:57:05 +0200

    Fixed syntax errors in util.impersonate

--- home/util.py
+++ home/util.py
@@ -3,11 +3,11 @@ from .views import *
 
 from django.http import HttpRequest
 
-def impersonate(username, chantcode, gabc, source, sourcepage, mode, differentia, comment)
+def impersonate(username, chantcode, gabc, source, sourcepage, mode, differentia, comment):
   """Allows the server shell to send a proposal creation/update in the name of a specific user
   username, chantcode : self-explanatory.
   gabc, source, sourcepage, mode, differentia, comment : human-readable strings as entered in a proposal_edit form."""
-  r = HttpRequest
+  r = HttpRequest()
   r.method = "POST"
   u = User.objects.get(username=username)
   r.user = u
