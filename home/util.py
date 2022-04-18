aa0a7b12fea1aeb1f1aad147d8abdea0e845eb36
Matthias Bry <marteo@marteo.fr>
2022-04-18 08:23:08 +0200

    Added an 'impersonate' function for posting proposals in the name of someone else, from the shell

---
+++ home/util.py
@@ -0,0 +1,22 @@
+from .models import *
+from .views import *
+
+from django.http import HttpRequest
+
+def impersonate(username, chantcode, gabc, source, sourcepage, mode, differentia, comment)
+  """Allows the server shell to send a proposal creation/update in the name of a specific user
+  username, chantcode : self-explanatory.
+  gabc, source, sourcepage, mode, differentia, comment : human-readable strings as entered in a proposal_edit form."""
+  r = HttpRequest
+  r.method = "POST"
+  u = User.objects.get(username=username)
+  r.user = u
+  r.POST["gabc"] = gabc
+  r.POST["source"] = source
+  r.POST["sourcepage"] = sourcepage
+  r.POST["mode"] = mode
+  r.POST["differentia"] = differentia
+  r.POST["comment"] = comment
+  edit_proposal(r, chantcode)
+
+
